741a18dbeda1c6bcb2316a1bfbf70ef4beaf8759
Gregory El Bajoury <145674649+GregoryElBajoury@users.noreply.github.com>
2023-09-21 14:41:06 +0200

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,1 +1,12 @@
+locals {
+  app_details = {
+    display_name = "GitHub-IAC"
+    account_type = false # Ceci assure que seuls les comptes de votre organisation peuvent accéder à cette application (équivalent à Single tenant)
+    oauth2_flow  = true
+  }
 
+  secret_details = {
+    description = "Client secret for automation"
+    duration    = "8760h"  # 365 days in hours
+  }
+}
