7d6bc1e2292d79f927419836282277a82932ecb6
Gregory El Bajoury <145674649+GregoryElBajoury@users.noreply.github.com>
2023-09-21 14:42:02 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "client_id" {
+  description = "Client ID de l'application Azure AD."
+  value = azuread_service_principal.example.application_id
+}
 
+output "client_secret" {
+  description = "Secret associé au Service Principal."
+  value = azuread_service_principal_password.example.value
+}
+
+output "tenant_id" {
+  description = "ID du tenant Azure AD actuellement utilisé."
+  value = data.azurerm_client_config.example.tenant_id
+}
